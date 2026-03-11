b56e50e00d4949f7db2323e1e331fb0b8cabb918
122334444 <145564481+122334444@users.noreply.github.com>
2026-03-11 20:00:28 +0530

    +1

---
+++ BST/construction.java
@@ -0,0 +1,50 @@
+package BST;
+
+import java.util.*;
+
+public class construction {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static Node construct(int arr[], int low, int hi) {
+        if (low > hi) {
+            return null;
+        }
+        int mid = (low + hi) / 2;
+        int data = arr[mid];
+        Node lc = construct(arr, low, mid - 1);
+        Node rc = construct(arr, mid + 1, hi);
+
+        Node node = new Node(data, lc, rc);
+        return node;
+    }
+
+    public static void display(Node node) {
+        if (node == null) {
+            return;
+        }
+        String str = "";
+        str += node.left != null ? node.left.data : ".";
+        str += " <- " + node.data + " -> ";
+        str += node.right != null ? node.right.data : ".";
+        System.out.println(str);
+        display(node.left);
+        display(node.right);
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 12, 25, 37, 50, 62, 75, 87 };
+        Node root = construct(arr, 0, arr.length - 1);
+        display(root);
+    }
+}
