697b83fd3892803055e5f9ab912c42dee7338a93
122334444 <145564481+122334444@users.noreply.github.com>
2026-03-11 20:43:50 +0530

    +1

--- BST/construction.java
+++ BST/construction.java
@@ -42,9 +42,51 @@ public class construction {
         display(node.right);
     }
 
+    public static boolean find(Node root, int val) {// in bst search travels only along the path in which target exist
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == val) {
+            return true;
+        } else if (root.data > val) {
+            return find(root.left, val);
+        } else {
+            return find(root.right, val);
+        }
+    }
+
+    public static int max(Node root) {
+        if (root.right != null) {
+            return max(root.right);
+        } else {
+            return root.data;
+        }
+    }
+
+    public static int min(Node root) {
+        if (root.left != null) {
+            return min(root.left);
+        } else {
+            return root.data;
+        }
+    }
+
+    public static int sum(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int ls = sum(root.left);
+        int rs = sum(root.right);
+        return ls + rs + root.data;
+    }
+
     public static void main(String args[]) {
         int arr[] = { 12, 25, 37, 50, 62, 75, 87 };
         Node root = construct(arr, 0, arr.length - 1);
         display(root);
+        System.out.println("Sum: " + sum(root));
+
+        System.out.println(find(root, 62));
     }
 }
