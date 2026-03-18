acd388d59958120a3950a86d6a42a7af703fc02b
122334444 <145564481+122334444@users.noreply.github.com>
2026-03-18 18:40:24 +0530

    +1

--- LinkedList/remove.java
+++ LinkedList/remove.java
@@ -29,12 +29,41 @@ public class remove {
         curr.next = null;
     }
 
+    public static void removeIdx(Node head, int idx) {
+        if (head == null) {
+            return;
+        }
+        if (idx == 0) {
+            head = head.next;
+            return;
+        }
+        Node curr = head;
+        for (int i = 0; i < idx - 1; i++) {
+            if (curr.next == null) {
+                return;
+            }
+            curr = curr.next;
+        }
+        if (curr.next == null) {
+            return;
+        }
+
+        curr.next = curr.next.next;
+    }
+
     public static void main(String args[]) {
         Node head = new Node(1);
         head.next = new Node(2);
         head.next.next = new Node(3);
         head.next.next.next = new Node(4);
         head.next.next.next.next = new Node(5);
-
+        head.next.next.next.next.next = new Node(6);
+        // removeLast(head);
+        removeIdx(head, 2);
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
     }
 }
