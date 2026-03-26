d57ee36c230ef197729553f49314c686b5a19c67
122334444 <145564481+122334444@users.noreply.github.com>
2026-03-27 01:03:15 +0530

    +1

---
+++ DP/dpOneDimention/climbingStairs.java
@@ -0,0 +1,21 @@
+package DP.dpOneDimention;
+
+///You are climbing a staircase. It takes n steps to reach the top. 
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+public class climbingStairs {
+    public static int climbStairs(int n) {
+        int a[] = new int[n + 1];
+        a[0] = 1;
+        a[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            a[i] = a[i - 1] + a[i - 2];
+        }
+        return a[n];
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        System.out.println(climbStairs(n));
+    }
+}
